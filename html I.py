dfe43a54d6e3ac4c6153ebafb43c580b10e150fa
FriedBallerina <FriedBallerina@users.noreply.github.com>
2021-01-13 01:17:53 +0000

    Add files via upload

---
+++ html I.py
@@ -0,0 +1,42 @@
+# Web Design I
+# HTML
+
+# Toronto Public Library
+# 3 main pillars of web design:
+# content = HTML, styles = CSS, and functionality = JS
+# End result of the course: a website from scratch
+
+# when saving a file in brackets (or other software)
+# add .html
+# brackets has live preview which is handy to
+# see the progress as you work on your file
+
+# some of the popular code editors: brackets, atom, vcs
+
+# html is about content, not looks
+# markup description: we take content and describe what the content type is
+# syntax of a programming language is similar to grammar in natural language
+# html has one of the simplest syntax sets of any coding languages
+#for the most part, every line of code starts with a tag which describes the content
+# the tag is enclosed in brackets. The content is inside the brackets.
+# Usually it is followed by a closing tag.
+
+# html 'boiler plate' = a template you always start with
+#<!DOCTYPE html>
+#<html lang="en">
+#<head>
+#    <meta charset="utf-8">
+#</head>
+#</html>
+
+# head = background information like keywords for search engines,
+#links to stylesheets etc.
+# body = holds the visual content the users can see
+# file -> extension manager -> beautify add-on is handy
+
+# <a href=#> placeholder, makes the link clickable but doesn't take you anywhere :)
+
+#   HOMEWORK:
+# think of smth you want to make a website about
+# take all of the sections and see if you can put in
+# your own content, footer etc. so you get a customised page.
