6500f0483d8b43dc66ff67a88eef5edb09213b8c
FriedBallerina <FriedBallerina@users.noreply.github.com>
2021-01-20 19:52:16 +0000

    Add files via upload

---
+++ css II.py
@@ -0,0 +1,17 @@
+# css reset: our own rules for css of the website
+# * is for all of the elements on the website
+# live preview error: open html file and live preview from there
+
+# notation for class in css-file is "."
+# notation for id is "#"
+
+# rem is based on default sizing
+
+# nav links: floats, css flex and css grids.
+# float: box model, gets rid of the actual box. It erases the box
+# and the rest of the text floats around it.
+# grid: a bit more complex, less automatic (look up css grids online)
+#css flex: automatic and easy to use with navigation bars
+
+#HOMEWORK: FLEX ARTICLES, OTHER OBJECTS
+#CREATE CLASSES
